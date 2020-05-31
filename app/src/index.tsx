71b2abad1aaf171f6a56e7a5bb75939d4f4dcb81
doong-jo <sdong001@gmail.com>
2020-05-31 19:27:07 +0900

    chore: apply prettier

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,11 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Server } from 'miragejs';
+
+new Server({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/movies', () => {
+      return {
+        movies: [
+          { id: 1, name: 'Inception', year: 2010 },
+          { id: 2, name: 'Interstellar', year: 2014 },
+          { id: 3, name: 'Dunkirk', year: 2017 },
+        ],
+      };
+    });
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change

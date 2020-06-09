62e007c73c4a9128f3bf0c580f7303f9801f1c13
doong-jo <sdong001@gmail.com>
2020-06-09 23:21:51 +0900

    refactor: remove ApiController and put in App

--- app/src/App.js
+++ app/src/App.js
@@ -1,103 +1,60 @@
 import React, { useState, useEffect } from 'react';
 
-import { ApiControllerView } from './ApiController';
+import { lodgingMapper } from './mapper';
+
+class ApiConfig {
+  url = '/mock/product';
+
+  setMockUrl() {
+    this.url = '/mock/product';
+  }
+
+  setRealUrl() {
+    this.url = 'http://api3.dev-myrealtrip.com/product';
+  }
+}
+
+const apiConfig = new ApiConfig();
 
 function App() {
-  const [product, setProduct] = useState();
-  const [products, setProducts] = useState();
-  const [order, setOrder] = useState();
-  const [orders, setOrders] = useState();
-  const [newOrder, setNewOrder] = useState();
+  const [isMock, setIsMock] = useState(true);
+  const [lodgingData, setLodgingData] = useState();
 
-  function loadOrders() {
-    fetch('/order/1')
-      .then((res) => res.json())
-      .then((json) => {
-        setOrder(json.order);
-      });
-    fetch('/order/list')
-      .then((res) => res.json())
-      .then((json) => {
-        setOrders(json.orders);
-      });
+  function setUrlMode(isMockMode) {
+    if (isMockMode) {
+      apiConfig.setMockUrl();
+    } else {
+      apiConfig.setRealUrl();
+    }
   }
 
   useEffect(() => {
-    fetch('/product/BNB1')
+    setUrlMode(isMock);
+
+    fetch(`${apiConfig.url}/lodging-home`)
       .then((res) => res.json())
       .then((json) => {
-        setProduct(json.product);
+        if (!isMock) {
+          setLodgingData(lodgingMapper(json));
+          return;
+        }
+        setLodgingData(json);
       });
-    fetch('/product/list')
-      .then((res) => res.json())
-      .then((json) => {
-        setProducts(json.products);
-      });
-    fetch('/order/1')
-      .then((res) => res.json())
-      .then((json) => {
-        setOrder(json.order);
-      });
-    fetch('/order/list')
-      .then((res) => res.json())
-      .then((json) => {
-        setOrders(json.orders);
-      });
-    fetch('/order/3', {
-      method: 'POST',
-      body: 'BNB3',
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        setNewOrder(json);
-        loadOrders();
-      });
-  }, []);
+  }, [isMock]);
 
   return (
-    <div style={{ marginLeft: '24px' }} className="App">
-      <ApiControllerView />
-      <h1>API TEST</h1>
-      <h2>GET /product/:id</h2>
-      {product && (
-        <ul>
-          <li>{product.id}</li>
-          <li>{product.name}</li>
-        </ul>
-      )}
-      <h2>GET /product/list</h2>
-      {products &&
-        products.map((product) => (
-          <ul key={product.id}>
-            <li>{product.id}</li>
-            <li>{product.name}</li>
-          </ul>
-        ))}
-      <h2>POST /order/:id</h2>
-      {newOrder && (
-        <ul>
-          <li>{newOrder.id}</li>
-          <li>{newOrder.productId}</li>
-        </ul>
-      )}
-      <h2>GET /order/:id</h2>
-      {order ? (
-        <ul>
-          <li>{order.id}</li>
-          <li>{order.productId}</li>
-        </ul>
-      ) : (
-        'pending..'
-      )}
-      <h2>GET /order/list</h2>
-      {orders
-        ? orders.map((order) => (
-            <ul key={order.id}>
-              <li>{order.id}</li>
-              <li>{order.productId}</li>
-            </ul>
-          ))
-        : 'pending..'}
+    <div>
+      <h1>API CONTROLLER</h1>
+      <h2>GET /lodging-home</h2>
+      <span style={{ margin: '0 10px' }}>{isMock ? 'mock' : 'real'}</span>
+      <button
+        onClick={() => {
+          setIsMock(!isMock);
+        }}
+      >
+        change
+      </button>
+      {lodgingData && <pre>{JSON.stringify(lodgingData, null, 4)}</pre>}
     </div>
   );
 }
